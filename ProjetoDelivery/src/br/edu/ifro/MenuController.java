55e4b7810c423b4b0c24c2367ffa52c39ba18148
03850980227 <03850980227@JIPA-LABIN04-03.ifro.local>
2018-03-16 09:39:37 -0400

    Modificando Menu

--- ProjetoDelivery/src/br/edu/ifro/MenuController.java
+++ ProjetoDelivery/src/br/edu/ifro/MenuController.java
@@ -16,9 +16,8 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
