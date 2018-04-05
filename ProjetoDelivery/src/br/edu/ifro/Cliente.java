ef729042d05438363426aaf6d28fce45faeb8d02
Geysson <geyssongg@gmail.com>
2018-04-05 15:08:20 -0400

    Instalação Hibernate

---
+++ ProjetoDelivery/src/br/edu/ifro/Cliente.java
@@ -0,0 +1,32 @@
+
+package br.edu.ifro;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+      public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    void setNome (String nome){
+         this.nome = nome;
+    }
+    private String getNome (){
+        return this.nome;
+    }
+    
+}
