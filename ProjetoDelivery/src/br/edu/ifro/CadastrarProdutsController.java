d1ba05af7f0e544924c0e35b06f08d8a4fc61dad
03186325277 <03186325277@JIPA-LABIN05-30.ifro.local>
2018-04-06 14:59:49 -0400

    Tela de produto

---
+++ ProjetoDelivery/src/br/edu/ifro/CadastrarProdutsController.java
@@ -0,0 +1,17 @@
+
+package br.edu.ifro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class CadastrarProdutsController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+    }    
+    
+}
