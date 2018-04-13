95ecf4405a1241d69d0faca2eb9e55f27c50683d
Insanittos <juniorrinco@hotmail.com>
2018-04-13 14:26:34 -0400

    funcionario controller

---
+++ ProjetoDelivery/src/br/edu/ifro/CadastrarFuncionarioController.java
@@ -0,0 +1,90 @@
+package br.edu.ifro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javax.swing.JOptionPane;
+
+
+public class CadastrarFuncionarioController implements Initializable {
+
+     @FXML
+    private Button btncancelar;
+    @FXML
+    private Button btnlimpar;
+    @FXML
+    private TextField txtcod;
+    @FXML
+    private TextField txtcpf;
+    @FXML
+    private TextField txtfuncao;
+    @FXML
+    private TextArea txtdatacontrato;
+    @FXML
+    private TextField txttelefone;
+    @FXML
+    private Button btnsalvar;
+    @FXML
+    private Button btnnome;
+    @FXML
+    private TextField txtdatanascimento;
+    @FXML
+    private TextField txtsalario;
+    @FXML
+    private TextField txtfuncao;
+     
+     
+     
+    
+     @FXML
+    private void exit(ActionEvent e) {
+        
+     if  (e.getSource() == btncancelar)
+	System.exit(0);
+    }
+    
+     
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+
+    @FXML
+    private void limpar(ActionEvent e) {
+        if(e.getSource()== btnlimpar) {
+            limpar(); 
+				}
+        
+    }
+
+    private void limpar() {
+   	txtcod.setText("");
+	txtprod.setText("");
+        txtquant.setText("");
+        txtvalor.setText("");
+        txtdesc.setText("");
+    
+}
+
+    @FXML
+    private void salvar(ActionEvent e) {
+        if (e.getSource()== btnsalvar) {
+	salvarprod();
+        
+    }
+    }
+
+    private void salvarprod() {
+        JOptionPane.showMessageDialog(null, "Cadastro concluido com Sucesso ");
+              limpar();
+			       
+        
+    }
+}
