5273e441e7187aadcc4351fa4afdc93a315ea6f1
Administrador <Administrador@LABORATORIO14>
2018-04-25 10:21:10 -0400

    tela de salvamento

--- ProjetoDelivery/src/br/edu/ifro/App.java
+++ ProjetoDelivery/src/br/edu/ifro/App.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -19,10 +20,11 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("CadastroProduts.fxml"));
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
      
