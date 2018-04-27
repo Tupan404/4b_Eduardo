a6112ab7c03e8e8f795ce05f0bf89c9c4bb57d0b
Geysson <geyssongg@gmail.com>
2018-04-27 15:20:36 -0400

    Trabalho feito na aula de 27/04

---
+++ ProjetoDelivery/src/br/edu/ifro/VisualizarClienteController.java
@@ -0,0 +1,71 @@
+
+package br.edu.ifro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class VisualizarClienteController implements Initializable {
+
+  @FXML
+  TextField txtnome;
+  @FXML
+  TextField txtcpf;
+  @FXML
+  TextField txtrg;
+  @FXML
+  TextField txtcelular;
+  @FXML
+  TextField txtemail;
+  @FXML
+  TextField txtcidade;
+  @FXML
+  TextField txtuf;
+  @FXML
+  TextField txtrua;
+  @FXML
+  TextField txtnum;
+  @FXML
+  TextField txtbairro;
+  @FXML
+  TextField txtcomp;
+  @FXML
+  Button alterarImagem;
+  @FXML
+  Button btnsalvar;
+  @FXML
+  Button btncancelar;
+  @FXML
+  Button btneditar;
+  @FXML
+  Button btnexcluir;
+
+  @FXML
+  public void salvar (){
+      
+  }
+  @FXML
+  public void cancelar (){
+      
+  }
+  @FXML
+  public void excluir (){
+      
+  }
+  @FXML
+  public void editar (){
+      
+  }
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+   
+    }    
+    
+ 
+    
+}
