bba5da6adc8a4a68a9d1d6e4b7e09f15b102b60a
03850980227 <03850980227@JIPA-LABIN05-24.ifro.local>
2018-05-04 14:51:40 -0400

    Desenvolvendo Calculadora

--- Grupo4_eduardo/src/br/edu/ifro/MenuPrincipalController.java
+++ Grupo4_eduardo/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,16 +22,29 @@ public class MenuPrincipalController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtNumero1;
+    @FXML
+    private TextField txtNumero2;
+    @FXML
+    private TextField txtResultado;
+    @FXML
+    private Button btnSoma;
+    
+    
+
+    @FXML
+    private void soma(ActionEvent event) {
+        Double num1 =Double.parseDouble(txtNumero1.getText());
+        Double num2 =Double.parseDouble(txtNumero2.getText());
+            Double resultado = num1 + num2;
+            
+            txtResultado.setText(resultado.toString());
     }
     
-    @Override
+     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
 }
