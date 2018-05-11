c520cb7f6d4007caa53c1c162fef81f3f5080e8f
03850980227 <03850980227@JIPA-LABIN05-24.ifro.local>
2018-05-11 15:26:02 -0400

    implementando Interface

--- ProjetoDelivery/src/br/edu/ifro/VisualizarClienteController.java
+++ ProjetoDelivery/src/br/edu/ifro/VisualizarClienteController.java
@@ -1,12 +1,21 @@
 
 package br.edu.ifro;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class VisualizarClienteController implements Initializable {
@@ -43,6 +52,15 @@ public class VisualizarClienteController implements Initializable {
   Button btneditar;
   @FXML
   Button btnexcluir;
+    @FXML
+    private Font x1;
+    @FXML
+    private Font x21;
+    @FXML
+    private Font x211;
+    private Label lblIamge;
+    @FXML
+    private ImageView Imagem;
 
   @FXML
   public void salvar (){
@@ -65,7 +83,34 @@ public class VisualizarClienteController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
    
     }    
+
+    @FXML
+    private void alterar(ActionEvent event) {
+        
+        
+        JFileChooser fileChooser = new JFileChooser();
+   
+        fileChooser.showOpenDialog(fileChooser);
+        fileChooser.setDialogTitle("Procurar Imagem");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Imagem", "jpg","png");
+        
+        fileChooser.setFileFilter(filter);
+        int retorno = fileChooser.showOpenDialog(fileChooser);
+        
+        if(retorno == JFileChooser.APPROVE_OPTION){
+            
+            File file = fileChooser.getSelectedFile();
+            
+            Image image = new Image("exit.png");            
+            Imagem.setImage(image);
+        }
+        
+        
+    }
+    }
     
  
-    
-}
+   
+
